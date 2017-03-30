7905f6a2a2beef4d07e64c4e21bd71fedc10e564
suming <suming@xiaogu-tech.com>
2017-03-30 19:49:11 +0800

    support news reply

--- plantDiseaseAI/server_for_demo/main.py
+++ plantDiseaseAI/server_for_demo/main.py
@@ -72,8 +72,10 @@ class FakeHandle:
                 return replyMsg.send()
             if recMsg.MsgType == 'image':
                 # 发送信息
-                content = self.process_txt('image')
-                replyMsg = reply.TextMsg(toUser, fromUser, content.encode('utf-8'))
+                # content = self.process_txt('image')
+                # replyMsg = reply.TextMsg(toUser, fromUser, content.encode('utf-8'))
+                # return replyMsg.send()
+                replyMsg = reply.NewsMsg(toUser, fromUser, 1, '这可能是苹果白粉病', '苹果白粉病（Apple powdery mildew）在我国苹果产区发生普遍。除为害苹果外，还为害梨、沙果、海棠、槟子和山定子等，对山定子实生苗、小苹果类的槟沙果、海棠和苹果中的倭锦、祝、红玉、国光等品种为害重。', 'http://g.hiphotos.baidu.com/baike/w%3D268%3Bg%3D0/sign=442e0372f2d3572c66e29bdab228041a/18d8bc3eb13533facc74c7b5a8d3fd1f40345b6e.jpg', 'http://baike.baidu.com/item/%E8%8B%B9%E6%9E%9C%E7%99%BD%E7%B2%89%E7%97%85/10248498?fr=aladdin')
                 return replyMsg.send()
             if recMsg.MsgType == 'voice':
                 content = '用户: ' + recMsg.FromUserName + ' 发送了语音，转化为文字为: ' + recMsg.Recognition
--- plantDiseaseAI/server_for_demo/reply.py
+++ plantDiseaseAI/server_for_demo/reply.py
@@ -32,6 +32,38 @@ class TextMsg(Msg):
         return XmlForm.format(**self.__dict)
 
 
+class NewsMsg(Msg):
+    def __init__(self, toUserName, fromUserName, count, title, desc, picurl, url):
+        self.__dict = dict()
+        self.__dict['ToUserName'] = toUserName
+        self.__dict['FromUserName'] = fromUserName
+        self.__dict['CreateTime'] = int(time.time())
+        self.__dict['ArticleCount'] = count
+        self.__dict['Title'] = title
+        self.__dict['Description'] = desc
+        self.__dict['PicUrl'] = picurl
+        self.__dict['Url'] = url
+
+    def send(self):
+        XmlForm = """
+        <xml>
+        <ToUserName><![CDATA[{ToUserName}]]></ToUserName>
+        <FromUserName><![CDATA[{FromUserName}]]></FromUserName>
+        <CreateTime>{CreateTime}</CreateTime>
+        <MsgType><![CDATA[news]]></MsgType>
+        <ArticleCount>{ArticleCount}</ArticleCount>
+        <Articles>
+        <item>
+        <Title><![CDATA[{Title}]]></Title>
+        <Description><![CDATA[{Description}]]></Description>
+        <PicUrl><![CDATA[{PicUrl}]]></PicUrl>
+        <Url><![CDATA[{Url}]]></Url>
+        </item>
+        </Articles>
+        </xml>
+        """
+        return XmlForm.format(**self.__dict)
+
 class ImageMsg(Msg):
     def __init__(self, toUserName, fromUserName, mediaId):
         self.__dict = dict()
