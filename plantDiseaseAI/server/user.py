3fd2de238ce5832064531963e785fdcfdd81f504
suming <suming@xiaogu-tech.com>
2017-04-17 10:50:40 +0800

    add version info to user profile json to check if data structure is valid

--- plantDiseaseAI/server/user.py
+++ plantDiseaseAI/server/user.py
@@ -15,27 +15,35 @@ max_history_length = 5
 
 jkey_images = 'images'
 jkey_quetions = 'questions'
+jkey_version = 'version'
+
+# 当前json格式版本，如果不同说明结构变化，当作新用户处理
+CURRENT_VERSION = '1.0'
 
 
 class UserInfo(object):
     def __init__(self, openid):
         fpath = join(json_path, openid + file_ext)
+        self.fpath = fpath
         if os.path.isfile(fpath):
             util.lstr('老用户openid: {}'.format(openid))
             with open(fpath) as f:
                 self.json_obj = json.load(f)
-                questions = list(q for q in self.json_obj[jkey_quetions])
-                self.json_obj[jkey_quetions] = deque(questions, maxlen=max_history_length)
-                images = list(img for img in self.json_obj[jkey_images])
-                self.json_obj[jkey_images] = deque(images, maxlen=max_history_length)
-        else:
-            util.lstr('新用户openid: {}'.format(openid))
-            self.json_obj = {}
-            self.json_obj[jkey_images] = deque([], maxlen=max_history_length)
-            self.json_obj[jkey_quetions] = deque([], maxlen=max_history_length)
-            self.reset_state()
+                if jkey_version in self.json_obj and self.json_obj[jkey_version] == CURRENT_VERSION:
+                    questions = list(q for q in self.json_obj[jkey_quetions])
+                    self.json_obj[jkey_quetions] = deque(questions, maxlen=max_history_length)
+                    images = list(img for img in self.json_obj[jkey_images])
+                    self.json_obj[jkey_images] = deque(images, maxlen=max_history_length)
+                    return
+                else:
+                    util.lstr('老用户json格式不是最新，重新生成')
 
-        self.fpath = fpath
+        util.lstr('新用户openid: {}'.format(openid))
+        self.json_obj = {}
+        self.json_obj[jkey_images] = deque([], maxlen=max_history_length)
+        self.json_obj[jkey_quetions] = deque([], maxlen=max_history_length)
+        self.reset_state()
+        self.json_obj[jkey_version] = CURRENT_VERSION
 
     def reset_state(self, status='Run'):
         session = WhiteBoard()
